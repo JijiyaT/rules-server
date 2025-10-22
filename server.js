878940fb94dacc413a70fb352aaa70aa65855a10
Lakshmi Thottempudi <lakshmi.thottempudi1@davita.com>
2025-10-22 15:16:57 +0530

    Updated configuration for Render deployment

--- server.js
+++ server.js
@@ -137,6 +137,15 @@ const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+// Security headers
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
